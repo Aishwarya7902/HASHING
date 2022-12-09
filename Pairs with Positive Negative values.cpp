3c48108fd4bca1a5805999635db7a0a1e3a1a651
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2022-12-09 08:53:19 -0800

    Update Pairs with Positive Negative values.cpp

--- Pairs with Positive Negative values.cpp
+++ Pairs with Positive Negative values.cpp
@@ -37,3 +37,34 @@ class Solution{
         return ans;
     }
 };
+
+/*
+METHOD 2:
+TC:
+SC:
+*/
+
+class Solution{
+  public:
+    vector<int> PosNegPair(int a[], int n) {
+       vector<int>v,ans;
+       map<int,int>mp;
+       for(int i=0;i<n;i++){
+           if(a[i]<0)
+            v.push_back(a[i]);
+           else
+            mp[a[i]]++;
+       }
+       sort(v.begin(),v.end());
+       for(int i=v.size()-1;i>=0;i--){
+           int data=abs(v[i]);
+           if(mp[data]>0){
+               ans.push_back(v[i]);
+               ans.push_back(data);
+               mp[data]--;
+           }
+       }
+       return ans;
+    }
+};
+
