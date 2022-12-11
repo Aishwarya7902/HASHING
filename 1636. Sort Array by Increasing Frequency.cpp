825aea31997b1a72884b7cc712b26d5c7d3c123e
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2022-12-11 08:28:59 -0800

    Create 1636. Sort Array by Increasing Frequency.cpp

---
+++ 1636. Sort Array by Increasing Frequency.cpp
@@ -0,0 +1,41 @@
+PROBLEM:
+VIDEO (PRINCE BHAIYA):
+
+/*
+method 1 (done on my own)
+TC:
+SC:
+*/
+
+
+class Solution {
+bool static comp(pair<int,int>&a,pair<int,int>&b){
+    if(a.second==b.second)
+     return a.first>b.first;
+    else
+     return a.second<b.second;
+}
+public:
+    vector<int> frequencySort(vector<int>& nums) {
+        vector<int>ans;
+        unordered_map<int,int>mp;
+        for(auto x:nums){
+            mp[x]++;
+        }
+        vector<pair<int,int>>p;
+        for(auto it:mp){
+            int freq=it.second;
+            int ele=it.first;
+            p.push_back(make_pair(ele,freq));
+        }
+        sort(p.begin(),p.end(),comp);
+        for(auto it:p){
+            int freq=it.second;
+            int ele=it.first;
+            vector<int>v(freq,ele);
+            ans.insert(ans.end(),v.begin(),v.end());
+        }
+        return ans;
+
+    }
+};
