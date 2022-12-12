eda532503222134665239aee5ab29ae8bd162309
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2022-12-12 10:55:18 -0800

    Update 149. Max Points on a Line Hard.cpp

--- KK BHAIYA/149. Max Points on a Line Hard.cpp
+++ KK BHAIYA/149. Max Points on a Line Hard.cpp
@@ -32,3 +32,52 @@ public:
      return res;   
     }
 };
+
+/*METHOD 2
+
+using hashmap
+Time Complexity : O(n^2)
+SC:O(n)
+*/
+
+class Solution {
+public:
+    int maxPoints(vector<vector<int>>& points) {
+     if(points.size()<=2)return points.size();
+     int res=0;
+     int n=points.size();
+     for(int i=0;i<n;i++){
+         unordered_map<double,int>mp;
+         double slope=0.0;
+         int duplicate=0;
+         for(int j=0;j<n;j++){
+             int x1=points[i][0];
+             int x2=points[j][0];
+             int y1=points[i][1];
+             int y2=points[j][1];
+             int dx=x2-x1;
+             int dy=y2-y1;
+             if(dx==0 && dy==0)
+             {
+              duplicate++;
+              continue;
+             }
+             if(dx!=0){
+                 slope=dy*1.0/dx;
+             }
+             else
+              slope=INT_MAX;
+
+          mp[slope]++;   
+         }
+         if(mp.size()==0)
+          res=duplicate;
+        else{
+            for(auto it:mp){
+                res=max(res,it.second+duplicate);
+            }
+        }
+     }
+     return res;   
+    }
+};
