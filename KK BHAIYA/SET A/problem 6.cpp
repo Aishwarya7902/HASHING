d315f3e1d2278ac7e74d937318ad578735addeab
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2022-12-13 05:33:13 -0800

    Update problem 6.cpp

--- KK BHAIYA/SET A/problem 6.cpp
+++ KK BHAIYA/SET A/problem 6.cpp
@@ -34,8 +34,8 @@ Output: true
   
   /*
   BRUTE FORCE
-  TC: O(n^2)
-  SC:
+  Time complexity: O(k*n)
+  Space complexity: O(1)
   */
 #include <iostream>
 using namespace std;
@@ -62,5 +62,50 @@ int main() {
         cout << "Yes";
     else
         cout << "No";
+}
+
+
+/*METHOD 2 :
+pseudo code:
+1.make an unordered map
+2.traverse array from left to right...then we will have two situations
+A.if current element is already present in map then get its index from map 
+calculate difference between current index and and index which we have fetched from map
+->if the difference is greater than k then  update the position of current element in the map
+->if difference is less than or equal to k then return true;
+
+B.if the array element is not present in map then just  insert with its index
+
+
+Time complexity: O(n)
+
+Space complexity: O(n)
+*/
+
+bool helpduplicateAtK(int arr[],int n,int k){
+    unordered_map<int,int>mp;
+    for(int i=0;i<n;i++){
+       if(mp.find(arr[i])!=mp.end()){
+           auto it=mp.find(arr[i]);
+           int indx=it->second;
+           if(i-indx<=k)return true;
+           else
+            mp[arr[i]]=i;
+       }
+       else
+            mp[arr[i]]=i;
+    }
+    return false;
+}
+int main()
+{
+    int arr[]={1,2,3,4,1,2,3,4};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    if(helpduplicateAtK(arr,n,3))
+     cout<<"Yes";
+    else
+    cout<<"No";
+
+    return 0;
 }
   
