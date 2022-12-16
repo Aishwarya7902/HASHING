ba98a9c4903a0e7e4d5713c016e0a994bca0d5c3
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2022-12-16 04:27:16 -0800

    Update problem 1.cpp

--- KK BHAIYA/SET B/problem 1.cpp
+++ KK BHAIYA/SET B/problem 1.cpp
@@ -30,3 +30,53 @@ class Solution{
         return cnt;
     }
 };
+
+/*
+METHOD 2 : using hashing
+TC: O(n)
+SC:O(n)
+Note:
+->prefixSum  will store sum till current index
+->count will store no of subarrays with sum==k
+->map will store count of subarrays corresponding to a particular sum
+
+intuition:
+1->take a count variable
+2->take a variable prefixSum to store sum till ith index
+3-> take an unordered map named countSum which will store count of subarrys corresponding to a particular sum
+for eg.
+mp[5]=2 , it means there are two subarrays whose sum is 5.
+4->run a loop from 0 to n-1
+5->add nums[i] to prefixSum
+6->if prefixsum==k ,count++;
+7->if (prefixsum-k) is already present in map then it means there is some subarray jiske sum ko prefixSum me se minus karenge to jo resultant
+sum aayega wo k ke barabar aayega.
+hence ! we got a subarray whose sum is k....increase count as much times this subarray is present
+8.mp[prefixSum]++; //as it can be used in future
+9.return count
+*/
+
+class Solution{
+    public:
+    int findSubArraySum(int nums[], int N, int k)
+    {
+        // code here
+        int count=0;
+        int prefixSum=0;
+        unordered_map<int,int>countSum;
+        for(int i=0;i<N;i++){
+            prefixSum+=nums[i];
+             if(prefixSum==k)
+              count++;
+            
+            if(countSum.find(prefixSum-k)!=countSum.end()){
+                    count+=countSum[prefixSum-k];
+                }
+            
+            countSum[prefixSum]++;
+        }
+     return count;   
+    }
+};
+
+
