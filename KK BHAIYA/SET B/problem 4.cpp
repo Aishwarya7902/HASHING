face424b51bf1e3a5fae11c9f9f0dbb15469fc4b
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2022-12-18 10:52:03 -0800

    Update problem 4.cpp

--- KK BHAIYA/SET B/problem 4.cpp
+++ KK BHAIYA/SET B/problem 4.cpp
@@ -24,3 +24,26 @@ public:
        return false;
     }
 };
+
+/*
+USING HASHING
+TC:O(N)
+SC:O(N)
+*/
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        unordered_map<int,int>mp;
+        int sum=0;
+        mp[0]=0;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+            if(mp.find(sum%k)==mp.end()){
+                mp[sum%k]=i+1;
+            }
+            else if(mp[sum%k]<i)
+             return true;
+        }
+        return false;
+    }
+};
