0327e2d2281be31790bb40a2861670a6d1e306c1
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2022-12-20 04:48:29 -0800

    Create problem 5.cpp

---
+++ KK BHAIYA/SET B/problem 5.cpp
@@ -0,0 +1,24 @@
+PROBLEM:https://leetcode.com/problems/subarray-sums-divisible-by-k/description/
+VIDEO:
+
+/*
+BRUTE FORCE
+TC:
+SC:
+PSEDO CODE
+*/
+
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+     int cnt=0;
+     for(int i=0;i<nums.size();i++){
+         int sum=0;
+         for(int j=i;j<nums.size();j++){
+             sum+=nums[j];
+             if(sum%k==0)cnt++;
+         }
+     } 
+     return cnt;  
+    }
+};
