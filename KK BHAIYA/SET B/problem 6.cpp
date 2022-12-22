03b9c5b3183d363bd2df6fe741051f589ed6721d
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2022-12-22 11:35:54 -0800

    Create problem 6.cpp

---
+++ KK BHAIYA/SET B/problem 6.cpp
@@ -0,0 +1,8 @@
+PROBLEM:https://www.geeksforgeeks.org/count-subarrays-with-sum-equal-to-its-xor-value/
+VIDEO:
+
+/*
+Naive Approach: The naive approach for this problem is to consider all the sub-arrays and for every subarray, check if the XOR is equal to the sum. 
+Time Complexity: O(N2)
+
+*/
