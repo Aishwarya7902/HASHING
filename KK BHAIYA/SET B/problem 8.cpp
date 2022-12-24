cd2751c95662e6788ad4a3a619fab1fadbc64a98
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2022-12-24 10:08:28 -0800

    Update problem 8.cpp

--- KK BHAIYA/SET B/problem 8.cpp
+++ KK BHAIYA/SET B/problem 8.cpp
@@ -23,3 +23,29 @@ public:
       return cnt;  
     }
 };
+
+/*
+USING HASHING
+blog:https://leetcode.com/problems/count-number-of-pairs-with-absolute-difference-k/solutions/1471324/c-simple-and-short-solution-using-map/?orderBy=most_votes
+TC:O(N)
+SC:O(N)
+INTUITION:
+1.In each iteration, we look how many numbers which have a k difference from our current number we have seen so far i.e we lool for k distance behind current no 
+and k distance beyond current no
+.
+Then we add the current number to the map....
+*/
+
+class Solution {
+public:
+    int countKDifference(vector<int>& nums, int k) {
+      int ans=0;
+      unordered_map<int,int>mp;
+      for(int i=0;i<nums.size();i++){
+          ans+=mp[nums[i]-k]+mp[nums[i]+k];
+          mp[nums[i]]++;
+    
+      } 
+      return ans; 
+    }
+};
