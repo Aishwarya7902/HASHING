f0ccb91db7a2428dcc788c04db933f1a7a0e33d5
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2022-12-30 07:22:50 -0800

    Update problem 9.cpp

--- KK BHAIYA/SET B/problem 9.cpp
+++ KK BHAIYA/SET B/problem 9.cpp
@@ -28,6 +28,19 @@ public:
 Using unordered map
 Time: O(n + mk) where where n is size of nums, m is target size and k is the length of the resulting substring.
 Space: O(n)
+1.map to store frequency of every string occuring in nums
+2. our target generate every possible pairs of sub string and check is that is present in map...if yes then nums[i]+nums[j]==target
+3. string s1=target.substr(0,i); //this will bring substing from target staring from index 0 to i-1
+   string s2=target.substr(i,n); //this will bring substing from target staring from index i to n-1
+4.if(s1==s2){
+  cnt+=mp[s1]*(mp[s2]-1);
+ }//....?????why this formula let say for string 77 we have frequecy 3 in map and in nums this 77 is at 
+ indices 0,1,2 hence the pair which we can form is ...{(0,1) ,(1,0),(0,2),(2,0),(1,2),(2,1)} 
+ hence cnt is 6 which is mp[77]=3 hence 3*(3-1)=6 hence formula proved
+5.else
+     cnt+=mp[s1]*mp[s2];
+6.return cnt;
+
 */
 
 #include<bits/stdc++.h>
