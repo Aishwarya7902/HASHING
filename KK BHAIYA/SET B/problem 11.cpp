1be1801a62264a3de39dba1a7a74cabeb64f2103
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2023-01-02 07:35:08 -0800

    Create problem 11.cpp

---
+++ KK BHAIYA/SET B/problem 11.cpp
@@ -0,0 +1,41 @@
+PROBLEM:https://leetcode.com/problems/4sum/
+VIDEO:https://www.youtube.com/watch?v=4ggF3tXIAp0
+
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>>ans;
+        int n=nums.size();
+        if(n==0)return ans;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+               long long int target2 = 1LL*target - 1LL*nums[ i ]  -  1LL*nums[ j ];
+                int low=j+1;
+                int high=n-1;
+                while(low<high){
+                    int twoSum=nums[low]+nums[high];
+                    if(twoSum<target2)
+                     low++;
+                    else if(twoSum>target2)
+                     high--;
+                    else{
+                        vector<int>quad(4,0);
+                        quad[0]=nums[i];
+                        quad[1]=nums[j];
+                        quad[2]=nums[low];
+                        quad[3]=nums[high];
+                       ans.push_back(quad);
+                       while(low<high && nums[low]==quad[2])low++;
+                       while(low<high && nums[high]==quad[3])high--;
+                    }
+                }
+                //processing j
+                while(j+1<n && nums[j+1]==nums[j])j++;
+            }
+            //processing i
+                while(i+1<n && nums[i+1]==nums[i])i++;
+        }
+        return ans;
+    }
+};
