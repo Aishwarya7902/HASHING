a820d33c508f720cfdd8a2b1f9bca5b694127b20
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2023-01-02 07:43:05 -0800

    Update problem 11.cpp

--- KK BHAIYA/SET B/problem 11.cpp
+++ KK BHAIYA/SET B/problem 11.cpp
@@ -1,6 +1,34 @@
 PROBLEM:https://leetcode.com/problems/4sum/
 VIDEO:https://www.youtube.com/watch?v=4ggF3tXIAp0
 
+/*
+PSEUDO CODE:
+1.sort
+2.two for loops i,j
+3.from j+1 .......low ,high loop
+4.if target2=target-nums[i]-nums[j]
+5.twoSum=nums[low]+nums[high]
+6.if(twoSum<target2)
+   low++;
+  else if(twoSum>target2)
+   high--;
+   else{
+      vector<int>quad(4,0);
+      quad[0]=nums[i];
+      quad[1]=nums[j];
+     quad[2]=nums[low];
+     quad[3]=nums[high];
+     ans.push_back(quad);
+     while(low<high && nums[low]==quad[2])low++;
+     while(low<high && nums[high]==quad[3])high--;
+     
+7.processing j
+       while(j+1<n && nums[j+1]==nums[j])j++;
+8.processing i
+      while(i+1<n && nums[i+1]==nums[i])i++;
+9.return ans;                    
+*/
+
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
